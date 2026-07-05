83ac50e899c774545d2d380ec8636157a457e5a1
Ganesh_Paravada <ganeshparavada7@gmail.com>
2026-07-05 07:34:31 +0530

    Add files via upload

---
+++ week-3/Spring REST using Spring Boot 3/Create a Spring Web Project using Maven/spring-learn/src/main/java/spring_learn/HelloController.java
@@ -0,0 +1,13 @@
+package spring_learn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello! Spring Boot is running successfully.";
+    }
+}
